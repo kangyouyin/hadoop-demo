422826345776653bb17a83622b1a38ee9b961960
kangyouyin <kangyouyin@jd.com>
2020-07-06 14:53:54 +0800

    test HiveJdbcClient

---
+++ src/main/java/com/kyy/hadoopdemo/hive/HiveJdbcClient.java
@@ -0,0 +1,30 @@
+package com.kyy.hadoopdemo.hive;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by kangyouyin on 2020/7/6.
+ */
+public class HiveJdbcClient {
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+
+    public static void main(String[] args) throws SQLException {
+        try {
+            Class.forName(driverName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Connection conn = DriverManager.getConnection("jdbc:hive2://node13:10000/test", "root", "123");
+        Statement stmt = conn.createStatement();
+        String sql = "select * from psn";
+        ResultSet res = stmt.executeQuery(sql);
+        while (res.next()) {
+            System.out.println(res.getString(1) + "-" + res.getString("name"));
+        }
+    }
+}
